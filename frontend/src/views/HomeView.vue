af05baa9868526352669ce5a284f554cea92c059
Nofmann <111362962+namfonn02@users.noreply.github.com>
2024-04-28 04:02:48 +0700

    update css
    edit css in frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -29,153 +29,86 @@ function getStatusText(status) {
 }
 </script>
 
-<style>
-/* table {
-  max-width: 12000x;
-} */
-
-/* th,
-td {
-  border: 2px solid #e2e8f0;
-  border-radius: 10px;
-} */
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-  max-width: 1300px;
-  margin: 0 auto;
-}
-th,
-td {
-  border: 2px solid #ffffff;
-
-  text-align: left;
-  padding: 8px;
-  text-align: center;
-}
-th {
-  font-size: 18px;
-  background-color: #f2f2f2;
-}
-h1 {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-tr {
-  height: 35px;
-}
-
-body {
-  background-image: url(image/img.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.itbkk-id {
-  font-size: 15px;
-}
-.itbkk-title {
-  font-size: 15px;
-}
-.itbkk-status {
-  font-size: 15px;
-}
-.itbkk-assingees {
-  font-size: 15px;
-}
-.title-web {
-  font-size: 40px;
-}
-/* .itbkk-item {
-  border-radius: 10px;
-} */
-</style>
-
 <template>
-  <div class="p-10 w-screen h-screen">
+  <div class="p-10 w-screen h-screen bg-gray-800">
     <div class="itbkk-us3 container mx-auto w-full max-w-screen-xl">
       <div class="flex justify-between items-center mb-4">
-        <h1
-          class="title-web text-3xl mb-5 text-slate-200 border-collapse text-center"
-          style="font-weight: 600"
-        >
+        <h1 class="mb-4 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-sky-400">
           IT-Bangmod Kradan Kanban
         </h1>
       </div>
 
-      <table class="w-full border-separate">
-        <thead class="bg-white-100 border-b-2 border-gray-400 text-purple-900">
-          <tr class="rounded-lg">
-            <th class="p-3 font-bold tracking-wide">Id</th>
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="md:w-full table-auto text-sm text-left rtl:text-right border-blue-300">
+          <thead class="text-lg text-white bg-blue-500 border-b border-blue-300">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-center tracking-wide">Id</th>
 
-            <th class="p-3 font-bold tracking-wide">Title</th>
+              <th scope="col" class="px-6 py-3 text-center tracking-wide">Title</th>
 
-            <th class="p-3 font-bold tracking-wide">Assignees</th>
+              <th scope="col" class="px-6 py-3 text-center tracking-wide">Assignees</th>
 
-            <th class="p-3 font-bold tracking-wide">Status</th>
-          </tr>
-        </thead>
+              <th scope="col" class="px-6 py-3 text-center tracking-wide">Status</th>
+            </tr>
+          </thead>
 
-        <tbody class="bg-white border-b-2 border-gray-400">
-          <tr class="itbkk-item" v-for="(task, index) in tasks" :key="index">
-            <!-- id -->
-            <td class="itbkk-id px-4 py-2 text-sm font-semibold text-center text-slate-700">
-              {{ task.id }}
-            </td>
+          <tbody>
+            <tr class="itbkk-item bg-blue-100 border-blue-300 hover:bg-blue-200" v-for="(task, index) in tasks" :key="index">
+              <!-- id -->
+              <th class="itbkk-id px-6 py-4 text-base text-blue-600 font-medium">
+                {{ task.id }}
+              </th>
 
-            <!-- Title -->
-            <td
-              class="itbkk-title px-4 py-2 text-sm font-semibold text-slate-700 whitespace-trim"
-              style="text-align: left"
-            >
-              {{ task.title }}
-            </td>
-            <!-- <td
-              class="itbkk-title px-4 py-2 text-sm font-semibold text-slate-700"
-              style="text-align: left"
-            >
-              {{ removeUnderscore(task.title) }}
-            </td> -->
+              <!-- Title -->
+              <td class="itbkk-title text-left">
+                <a href="#" class=" px-6 py-4 font-medium text-base text-blue-600 hover:underline">{{ task.title.trim() }}</a>
+              </td>
 
-            <!-- Assignees -->
-            <!-- <td
-              class="itbkk-assingees px-4 py-2 text-sm font-semibold text-center"
-              style="text-align: left; color: {{ displayAssignees(task.assignees) === 'Unassigned' ? 'gray' : 'black' }}; font-style: italic;"
-            >
-              {{ displayAssignees(task.assignees) }}
-            </td> -->
+              <td class="itbkk-assignees px-6 py-4 text-center text-base text-blue-600 font-medium"
+              :class="{ 'text-gray-500 italic': !task.assignees }">
+                {{ task.assignees || 'Unassigned' }}
+              </td>
 
-            <td
-              class="itbkk-assignees px-4 py-2 text-sm text-back font-semibold text-center"
-              :class="{ 'text-[gray] italic': !task.assignees }"
-            >
-              {{ task.assignees || 'Unassigned' }}
-            </td>
+              <!-- Status -->
+              
+              <td class="itbkk-status">
+                <div class="border-4 border-blue-100 rounded-3xl p-8 px-4 py-2 text-base text-white font-semibold text-center"
+                  :class="{
+                    'bg-red-400': getStatusText(task.status) === 'No Status',
+                    'bg-purple-400': getStatusText(task.status) === 'To Do',
+                    'bg-yellow-400': getStatusText(task.status) === 'Doing',
+                    'bg-green-500': getStatusText(task.status) === 'Done'
+                    }">
+                      {{ getStatusText(task.status) }}
+                </div>  
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-            <!-- Status -->
+        <div v-if="!tasks.length">
+          <p class="text-center text-gray-500 text-base font-medium bg-white h-10 items-center flex justify-center">
+            no tasks
+          </p>
+        </div>
 
-            <td
-              class="itbkk-status px-4 py-2 text-sm font-bold text-white text-center border border-gray-300 rounded p-2"
-              :class="{
-                'bg-red-700': getStatusText(task.status) === 'No Status',
-                'bg-blue-500': getStatusText(task.status) === 'To Do',
-                'bg-yellow-400': getStatusText(task.status) === 'Doing',
-                'bg-green-700': getStatusText(task.status) === 'Done'
-              }"
-            >
-              {{ getStatusText(task.status) }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <dialog id="my_modal_4" class="modal">
+          <div class="modal-box w-11/12 max-w-5xl">
+            <h3 class="font-bold text-lg">Hello!</h3>
+            <p class="py-4">Click the button below to close</p>
 
-      <div v-if="!tasks.length">
-        <p class="text-center text-gray-500 text-xl bg-white h-10 items-center flex justify-center">
-          no tasks
-        </p>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button, it will close the modal -->
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+
       </div>
+
     </div>
   </div>
+
 </template>
