e7995a8662684c526e5d2ff981fe30e4ce64f1ac
nofmann <111362962+namfonn02@users.noreply.github.com>
2024-04-28 04:10:17 +0700

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -91,24 +91,8 @@ function getStatusText(status) {
             no tasks
           </p>
         </div>
-
-        <dialog id="my_modal_4" class="modal">
-          <div class="modal-box w-11/12 max-w-5xl">
-            <h3 class="font-bold text-lg">Hello!</h3>
-            <p class="py-4">Click the button below to close</p>
-
-            <div class="modal-action">
-              <form method="dialog">
-                <!-- if there is a button, it will close the modal -->
-                <button class="btn">Close</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
-
+        
       </div>
-
     </div>
   </div>
-
 </template>
