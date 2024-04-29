5c0cc3c2a51631571ee6d99f8e049290e729b37b
sysadmin <methus.dumr@kmutt.ac.th>
2024-04-29 18:37:06 +0700

    Deploy bugfix

--- backend/src/main/java/sit/int221/backend/controllers/TaskController.java
+++ backend/src/main/java/sit/int221/backend/controllers/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://")
+@CrossOrigin
 @RequestMapping("/v1/tasks")
 public class TaskController {
     @Autowired
