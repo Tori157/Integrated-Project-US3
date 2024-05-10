30294790f71c5ac2ff7466cb93e358d991d087a9
Tori157 <puttinat2546@gmail.com>
2024-05-11 02:50:04 +0700

    Commit Edit backend
    --v new for test

--- backend/src/main/java/sit/int221/backend/dtos/AddEditTaskDTO.java
+++ backend/src/main/java/sit/int221/backend/dtos/AddEditTaskDTO.java
@@ -5,6 +5,7 @@ import sit.int221.backend.entities.TaskStatus;
 
 @Data
 public class AddEditTaskDTO {
+    private int id;
     private String title;
     private String description;
     private String assignees;
