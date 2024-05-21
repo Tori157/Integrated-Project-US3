068a713cb4b4e15bfc67723cbe3cbbb12010a2c7
nofmann <111362962+namfonn02@users.noreply.github.com>
2024-05-21 13:02:01 +0700

    Update dbscript.sql
    fix database for filter test

--- database/dbscript.sql
+++ database/dbscript.sql
@@ -19,12 +19,12 @@ CREATE TABLE IF NOT EXISTS tasks (
   updatedOn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB;
 
--- insert data to tasks table 
+-- insert data io tasks table 
 INSERT INTO tasks (title, description, assignees, status, createdOn, updatedOn)
-VALUES ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 'NO_STATUS', STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s')),
-('Repository', '', '', 'TO_DO', STR_TO_DATE('22-4-2024 09:05:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 14:00:00', '%d-%m-%Y %H:%i:%s')),
-('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', 'DOING', STR_TO_DATE('22-4-2024 09:10:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('25-4-2024 00:00:00', '%d-%m-%Y %H:%i:%s')),
-(' _Infrastructure_ ', ' _Setup containers_ ', 'ไก่งวง กับ เพนกวิน', 'DONE', STR_TO_DATE('22-4-2024 09:15:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 10:00:00', '%d-%m-%Y %H:%i:%s'));
+VALUES (TRIM('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0'), TRIM('Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5'), 'Assignees1Assignees2Assignees3', 'NO_STATUS', STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s')),
+(TRIM('Repository'), TRIM(''), '', 'TO_DO', STR_TO_DATE('22-4-2024 09:05:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 14:00:00', '%d-%m-%Y %H:%i:%s')),
+(TRIM('ดาต้าเบส'), TRIM('ສ້າງຖານຂໍ້ມູນ'), 'あなた、彼、彼女 (私ではありません)', 'DOING', STR_TO_DATE('22-4-2024 09:10:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('25-4-2024 00:00:00', '%d-%m-%Y %H:%i:%s')),
+(TRIM(' _Infrastructure_ '), TRIM(' _Setup containers_ '), 'ไก่งวง กับ เพนกวิน', 'DONE', STR_TO_DATE('22-4-2024 09:15:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 10:00:00', '%d-%m-%Y %H:%i:%s'));
 
 -- select tasks
 SELECT * FROM tasks order by createdOn asc;
@@ -36,12 +36,20 @@ CREATE TABLE IF NOT EXISTS statuses (
   description VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ) ENGINE = InnoDB;
 
-insert data to status table
-INSERT INTO statuses (name,description) 
-VALUES ('No Status', 'The default status'),
-('To Do', null),
-('Doing', 'Being worked on'),
-('Done', 'Finished');
+-- insert data to status table
+INSERT INTO statuses (name,description) VALUES ('No Status', 'The default status');
+INSERT INTO statuses (name,description) VALUES ('To Do', null);
+INSERT INTO statuses (name,description) VALUES ('Doing', 'Being worked on');
+INSERT INTO statuses (name,description) VALUES ('Done', 'Finished');
+
+
+INSERT INTO statuses (name,description) VALUES ('No Status', 'A status has not been assigned');
+INSERT INTO statuses (name,description) VALUES ('To Do', 'The task is included in the project');
+INSERT INTO statuses (name,description) VALUES ('In Progress', 'The task is being worked on');
+INSERT INTO statuses (name,description) VALUES ('Reviewing', 'The task is being reviewed');
+INSERT INTO statuses (name,description) VALUES ('Testing', 'The task is being tested');
+INSERT INTO statuses (name,description) VALUES ('Waiting', 'The task is waiting for a resource');
+INSERT INTO statuses (name,description) VALUES ('Done', 'The task has been completed');
 
 -- create tasksV2 table 
 CREATE TABLE IF NOT EXISTS tasksV2 (
@@ -56,11 +64,14 @@ CREATE TABLE IF NOT EXISTS tasksV2 (
 ) ENGINE = InnoDB;
 
 -- insert data to tasksV2 table
+
 INSERT INTO tasksV2 (title, description, assignees, statusId, createdOn, updatedOn)
-VALUES ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 1,STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 09:00:00', '%d-%m-%Y %H:%i:%s')),
-('Repository', '', '', 2, STR_TO_DATE('22-4-2024 09:05:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 14:00:00', '%d-%m-%Y %H:%i:%s')),
-('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', 3, STR_TO_DATE('22-4-2024 09:10:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('25-4-2024 00:00:00', '%d-%m-%Y %H:%i:%s')),
-(' _Infrastructure_ ', ' _Setup containers_ ', 'ไก่งวง กับ เพนกวิน', 4, STR_TO_DATE('22-4-2024 09:15:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('22-4-2024 10:00:00', '%d-%m-%Y %H:%i:%s'));
+VALUES ('NS01', '', '', 1,STR_TO_DATE('14-5-2024  09:00:00 ', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024  09:00:00', '%d-%m-%Y %H:%i:%s')),
+('TD01', '', '', 2,STR_TO_DATE('14-5-2024  09:10:00 ', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024  09:10:00', '%d-%m-%Y %H:%i:%s')),
+('IP01', '', '', 3,STR_TO_DATE('14-5-2024  09:20:00 ', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024  09:20:00', '%d-%m-%Y %H:%i:%s')),
+('TD02', '', '', 2,STR_TO_DATE('14-5-2024 09:30:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024 09:30:00', '%d-%m-%Y %H:%i:%s')),
+('DO01', '', '', 7,STR_TO_DATE('14-5-2024 09:40:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024 09:40:00', '%d-%m-%Y %H:%i:%s')),
+('IP02', '', '', 3,STR_TO_DATE('14-5-2024 09:50:00', '%d-%m-%Y %H:%i:%s'), STR_TO_DATE('14-5-2024 09:50:00', '%d-%m-%Y %H:%i:%s'));
 
 SELECT * FROM tasksV2 order by createdOn asc;
 SELECT * FROM statuses;
