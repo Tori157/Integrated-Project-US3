deafd741ae3be7c483fd7b1b6380426d8ff33302
Methus Dumrongmongkholkul <methus.dumr@kmutt.ac.th>
2024-09-03 01:37:09 +0700

    Add token authentication (may need to refresh once)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+let token = null
+
+const accessToken = document.cookie.match(/access_token=([^;]*)/)
+if (accessToken) {
+    token = accessToken[1]
+    window.fetch = ((originalFetch) => {
+        return (...args) => {
+        const headers = new Headers(args[1] && args[1].headers ? args[1].headers : {})
+        headers.set('Authorization', `Bearer ${token}`)
+        return originalFetch(...args, { headers })
+        }
+  })(window.fetch)
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
